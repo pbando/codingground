ae2c6b89361d1edadc2da873c7394c2854c9acab
pbando <pbando@hotmail.com>
2016-09-25 13:14:43 +0200

    Created by Coding Ground Client

---
+++ New Project-20160925/main.py
@@ -0,0 +1,32 @@
+# Hello World program in Python
+    
+
+    
+# Function definition is here
+def printList( a ):
+    i=0
+    while i < 4:
+        print a[i]
+        i=i+1
+    return
+
+# Hello World program in Python
+a = "A","B","C","D"   
+b = [0,0,0]
+
+def egyforma(lista):
+    lista
+
+for i in range (0,3):
+    b[0]=a[i]
+    for j in range (0,3):
+       if (i!=j):    
+           b[1]=a[j] 
+           for k in range (0,3):
+                if (j!=k):
+                    if (k!=i):
+                        b[2]=a[k] 
+                        print b
+
+
+    
